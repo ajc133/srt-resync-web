3ece0d082c45c99e27435897d3e28adbfdff4644
AJ Collins <16407457+ajc133@users.noreply.github.com>
2025-02-02 16:03:23 -0800

    clean up srt_resync.py

--- srt_resync.py
+++ srt_resync.py
@@ -1,38 +1,17 @@
 """
-Copyright (c) 2013, William Ting
-
-*  This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-*  This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-*  You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-  Original code: https://github.com/wting/srt-resync
-      Issue: the file in the repo doesn't contain any python format, so in
-      environments like Windows it can't run without modifying its extension
-      to srt-resync.py
-
-This script was copied from
+Original bash script: https://github.com/wting/srt-resync
+Copied from
 https://ourcodeworld.com/articles/read/968/how-to-re-synchronize-shift-subtitles-of-a-movie-from-a-srt-file-in-python-3
+
+Then it was updated by AJ Collins to export a function that updates a line at a time
 """
 
 import argparse
 import datetime
 import os
 import re
-import shutil
 import sys
-from io import FileIO as file
-from pathlib import Path
-from typing import Generator, Any
+from io import FileIO
 
 VERSION = "0.1"
 
@@ -45,7 +24,7 @@ def parse_options():
     parser.add_argument(
         "offset", type=float, help="Add this many seconds to each timestamp"
     )
-    parser.add_argument("srt_file", type=file)
+    parser.add_argument("srt_file", type=FileIO)
     parser.add_argument(
         "--version",
         action="version",
